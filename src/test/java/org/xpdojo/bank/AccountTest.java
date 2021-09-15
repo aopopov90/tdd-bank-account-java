8b74009e9c96830170cfbb1c8b648de41781ab69
antonpopov <aopopov90@gmail.com>
2021-09-15 13:36:51 +0100

    withdraw

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -2,14 +2,31 @@ package org.xpdojo.bank;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.hamcrest.Matchers.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountTest {
 
     @Test
-	@Disabled
+    public void initialAmountIsZero() {
+        Account account = new Account();
+        assertThat(account.balance).isEqualTo(0);
+    }
+
+    @Test
     public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+        Account account = new Account();
+        account.deposit(10);
+        account.deposit(20);
+        assertThat(account.balance).isEqualTo(30);
+    }
+
+    @Test
+    public void withdrawAnAmountToDecreaseTheBalance() {
+        Account account = new Account();
+        account.deposit(20);
+        account.withdraw(10);
+        assertThat(account.balance).isEqualTo(10);
     }
 }
